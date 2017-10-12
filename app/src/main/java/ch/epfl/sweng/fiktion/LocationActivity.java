177780864d10a5fb813117d207239efbdb4eee35
Alexandre Chau <alexandre.chau@epfl.ch>
2017-10-13 01:31:26 +0900

    Implemented GMaps UI

---
+++ app/src/main/java/ch/epfl/sweng/fiktion/LocationActivity.java
@@ -0,0 +1,45 @@
+package ch.epfl.sweng.fiktion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // enable zoom controls
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        // Add on position and move the camera
+        LatLng myPos = new LatLng(46.5187386, 6.5653137);
+        mMap.addMarker(new MarkerOptions().position(myPos).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPos));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+    }
+}
--- app/src/main/java/ch/epfl/sweng/fiktion/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/MainActivity.java
@@ -1,12 +1,20 @@
-package ch.epfl.sweng.bootcamp;
+package ch.epfl.sweng.fiktion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* Starts the map location activity */
+    public void startNearbyActivity(View view) {
+        Intent mapIntent = new Intent(this, LocationActivity.class);
+        startActivity(mapIntent);
     }
 }
