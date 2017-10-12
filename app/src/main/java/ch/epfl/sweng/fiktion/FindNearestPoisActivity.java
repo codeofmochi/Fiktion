36c68b372d78fd67e90ad01e6b93984e80a012c4
Christoph Rueff <christoph.rueff@gmail.com>
2017-10-12 14:38:49 +0200

    Add SeekBar in findnearestPOIS with radius indicator and start search button

--- app/src/main/java/ch/epfl/sweng/fiktion/FindNearestPoisActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/FindNearestPoisActivity.java
@@ -2,12 +2,39 @@ package ch.epfl.sweng.fiktion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class FindNearestPoisActivity extends AppCompatActivity {
 
+    private int p = 50;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_nearest_pois);
+
+        // SeekBar for search radius
+        SeekBar searchRadius = (SeekBar)findViewById(R.id.searchRadius);
+        final TextView radiusSelect = (TextView)findViewById(R.id.radiusSelect);
+
+        searchRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                radiusSelect.setText(String.valueOf(progress));
+                p = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
+
+
+
 }
