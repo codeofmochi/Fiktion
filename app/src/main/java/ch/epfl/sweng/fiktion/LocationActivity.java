d8ba2bad5160558815a93f823fbc96059cb1250a
Alexandre Chau <alexandre.chau@epfl.ch>
2017-10-13 22:54:21 +0900

    Implemented user locatino on google maps

--- app/src/main/java/ch/epfl/sweng/fiktion/LocationActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/LocationActivity.java
@@ -1,6 +1,9 @@
 package ch.epfl.sweng.fiktion;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -8,11 +11,17 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    // Google Maps
     private GoogleMap mMap;
+    // My location
+    private Location myPosition;
+    // Marker on Google Maps
+    private Marker myPositionMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +41,28 @@ public class LocationActivity extends AppCompatActivity implements OnMapReadyCal
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // enable zoom controls
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        // Add on position and move the camera
-        LatLng myPos = new LatLng(46.5187386, 6.5653137);
-        mMap.addMarker(new MarkerOptions().position(myPos).title("My Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPos));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
 
+        // check permissions
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        // enable my position
+        mMap.setMyLocationEnabled(true);
+        // listen on location change
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location arg0) {
+                // update my position
+                myPosition = arg0;
+                // update position marker
+                if (myPositionMarker != null) myPositionMarker.remove();
+                myPositionMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("My Position"));
+                // update camera
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(myPosition.getLatitude(), myPosition.getLongitude())));
+                mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+            }
+        });
     }
 }
