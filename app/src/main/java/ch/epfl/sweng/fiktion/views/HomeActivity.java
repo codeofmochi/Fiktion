2a3434fad9bf3a43362011a3559ae52bf0094094
Alexandre Chau <alexandre.chau@epfl.ch>
2017-10-18 18:52:04 +0900

    Started implementing Home UI

--- app/src/main/java/ch/epfl/sweng/fiktion/views/HomeActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/HomeActivity.java
@@ -2,10 +2,11 @@ package ch.epfl.sweng.fiktion.views;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import ch.epfl.sweng.fiktion.R;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
