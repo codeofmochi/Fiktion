ca713cecc2ee264cc42dc490af8f2af8acc3a977
Christoph Rueff <christoph.rueff@gmail.com>
2017-10-19 17:43:57 +0200

    Add front interface for adding a point of interest

--- app/src/main/java/ch/epfl/sweng/fiktion/views/AddPOIActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/AddPOIActivity.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.fiktion.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.ListAdapter;
 
@@ -13,46 +14,9 @@ import ch.epfl.sweng.fiktion.R;
 
 public class AddPOIActivity extends AppCompatActivity {
 
-    private ExpandableListView listView;
-    private FictionInputExpandableListAdapter fIListAdapter;
-    private List<String> listDataHeader;
-    private HashMap<String,List<String>> listHash;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_poi);
-
-        listView = (ExpandableListView)findViewById(R.id.add_poi_added_fictions);
-        initData();
-        fIListAdapter = new FictionInputExpandableListAdapter(this,listDataHeader,listHash);
-        listView.setAdapter(fIListAdapter);
-    }
-
-    private void initData() {
-        listDataHeader = new ArrayList<>();
-        listHash = new HashMap<>();
-
-        listDataHeader.add("Fictions Added");
-
-        List<String> fictionsAdded = new ArrayList<>();
-        fictionsAdded.add("test1");
-        fictionsAdded.add("test2");
-        fictionsAdded.add("test3");
-        fictionsAdded.add("test4");
-        fictionsAdded.add("test5");
-        fictionsAdded.add("test6");
-        fictionsAdded.add("test7");
-        fictionsAdded.add("test8");
-        fictionsAdded.add("test9");
-        fictionsAdded.add("test10");
-        fictionsAdded.add("test11");
-        fictionsAdded.add("test12");
-        fictionsAdded.add("test13");
-        fictionsAdded.add("test14");
-        fictionsAdded.add("test15");
-        fictionsAdded.add("test16");
-
-        listHash.put(listDataHeader.get(0), fictionsAdded);
     }
 }
