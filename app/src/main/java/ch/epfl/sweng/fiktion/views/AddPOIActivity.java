2e116af0eb37bde43adc9a571f6a6e9d89304f12
Christoph Rueff <christoph.rueff@gmail.com>
2017-10-19 21:26:05 +0200

    Remove myPosition button from the add POI interface

--- app/src/main/java/ch/epfl/sweng/fiktion/views/AddPOIActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/AddPOIActivity.java
@@ -1,12 +1,24 @@
 package ch.epfl.sweng.fiktion.views;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.InterpolatorRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +30,12 @@ import static ch.epfl.sweng.fiktion.views.GetLocationFromMapActivity.NEW_POI_LAT
 import static ch.epfl.sweng.fiktion.views.GetLocationFromMapActivity.NEW_POI_LONGITUDE;
 
 public class AddPOIActivity extends AppCompatActivity {
+
+    private Button myLocationButton;
+    private TextView longitudeView;
+    private TextView latitudeView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
