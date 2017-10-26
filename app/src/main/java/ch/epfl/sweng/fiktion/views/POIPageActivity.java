c0dce60297b37b34816c37cfe2b885c5823781ef
Alexandre Chau <alexandre.chau@epfl.ch>
2017-10-27 00:51:49 +0900

    Add vote button and map fragment (buggy)

--- app/src/main/java/ch/epfl/sweng/fiktion/views/POIPageActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/POIPageActivity.java
@@ -7,18 +7,30 @@ import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import ch.epfl.sweng.fiktion.R;
 import ch.epfl.sweng.fiktion.models.PointOfInterest;
 import ch.epfl.sweng.fiktion.providers.DatabaseProvider;
 import ch.epfl.sweng.fiktion.providers.Providers;
 
-public class POIPageActivity extends MenuDrawerActivity {
+public class POIPageActivity extends MenuDrawerActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // give layout to parent menu class
         includeLayout = R.layout.activity_poipage;
         super.onCreate(savedInstanceState);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         // get POI name
         Intent from = getIntent();
@@ -48,4 +60,15 @@ public class POIPageActivity extends MenuDrawerActivity {
         span.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorPrimary)), 12, featured.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         featured.setText(span);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 }
