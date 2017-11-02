8f2f47d015c66188413c69ce3dd772dc28801c8f
Alexandre Chau <accounts@chau.moe>
2017-11-02 23:11:30 +0100

    Add file structure for Wikipedia position

---
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/GetLocationFromWikipediaActivity.java
@@ -0,0 +1,15 @@
+package ch.epfl.sweng.fiktion.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.epfl.sweng.fiktion.R;
+
+public class GetLocationFromWikipediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_location_from_wikipedia);
+    }
+}
