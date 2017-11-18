8985d53b0be87352ffc1dd7bd5aeae8d4c4473df
Alexandre Chau <alexandre.chau@epfl.ch>
2017-11-18 21:09:06 +0100

    Implement search in UI with basic results infos

--- app/src/main/java/ch/epfl/sweng/fiktion/views/TextSearchActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/TextSearchActivity.java
@@ -1,20 +1,134 @@
 package ch.epfl.sweng.fiktion.views;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import ch.epfl.sweng.fiktion.R;
+import ch.epfl.sweng.fiktion.models.PointOfInterest;
+import ch.epfl.sweng.fiktion.providers.DatabaseProvider;
+import ch.epfl.sweng.fiktion.providers.DatabaseSingleton;
 import ch.epfl.sweng.fiktion.views.parents.MenuDrawerActivity;
 
 public class TextSearchActivity extends MenuDrawerActivity {
+
+    private EditText searchField;
+    private LinearLayout resultsList;
+    private Context ctx = this;
+    private TextView noResults;
+    private final int SEARCH_TIMEOUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         includeLayout = R.layout.activity_text_search;
         super.onCreate(savedInstanceState);
 
+        // adjust background color
+        ScrollView bg = (ScrollView) findViewById(R.id.menu_scroll);
+        bg.setBackgroundColor(getResources().getColor(R.color.bgLightGray));
+
         // get search text
         Intent i = getIntent();
         String searchText = i.getStringExtra("SEARCH_TEXT");
+
+        // find search box
+        searchField = (EditText) findViewById(R.id.searchText);
+        searchField.setText(searchText);
+
+        // find results list
+        resultsList = (LinearLayout) findViewById(R.id.resultsList);
+
+        // find no results text
+        noResults = (TextView) findViewById(R.id.noResults);
+
+        // search
+        search(searchText);
+    }
+
+    /**
+     * Searches for given text and updates UI when results are found
+     *
+     * @param text the text to search POIs for
+     */
+    private void search(String text) {
+        // clear previous results
+        if (resultsList.getChildCount() > 0) resultsList.removeAllViews();
+        // check and alert if search field is empty
+        if (text.isEmpty()) {
+            searchField.setError(getString(R.string.empty_search));
+            Toast.makeText(ctx, "Search field is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // perform search
+        DatabaseSingleton.database.searchByText(text, new DatabaseProvider.SearchPOIByTextListener() {
+            @Override
+            public void onNewValue(PointOfInterest poi) {
+                // create new view for this POI
+                TextView tv = new TextView(ctx, null);
+
+                /* styles */
+                // background color
+                tv.setBackgroundColor(getResources().getColor(R.color.white));
+                // margin
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                params.setMargins(2, 10, 2, 2);
+                tv.setLayoutParams(params);
+                // padding
+                tv.setPadding(10, 10, 10, 10);
+                // shadow
+                tv.setElevation(2);
+                // text size
+                tv.setTextSize(14);
+
+                // set attributes
+                tv.setText(poi.name());
+
+                // add it to the results list
+                resultsList.addView(tv);
+
+                // we found a POI so update no results message
+                if (noResults.getVisibility() == View.VISIBLE)
+                    noResults.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onFailure() {
+                // something went wrong, show error toast
+                String err = "Request failed, please try again later.";
+                Toast.makeText(ctx, err, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // show loading text (will be hidden if new result, and replaced by no results if nothing shows up)
+        noResults.setText("Loading...");
+        if (noResults.getVisibility() == View.INVISIBLE) noResults.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        noResults.setText("No results found");
+                    }
+                },
+                SEARCH_TIMEOUT
+        );
+    }
+
+    /**
+     * Triggered by search button press
+     *
+     * @param view
+     */
+    public void triggerSearch(View view) {
+        String searchText = searchField.getText().toString();
+        // search
+        search(searchText);
     }
 }
