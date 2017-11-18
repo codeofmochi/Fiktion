7f59040fcb955bd58d19af9159263b2427add921
Alexandre Chau <alexandre.chau@epfl.ch>
2017-11-18 19:05:01 +0100

    Adapt UI to handle search

--- app/src/main/java/ch/epfl/sweng/fiktion/views/TextSearchActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/TextSearchActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import ch.epfl.sweng.fiktion.R;
 
-public class SearchByTextActivity extends AppCompatActivity {
+public class TextSearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_by_text);
+        setContentView(R.layout.activity_text_search);
     }
 }
