ef07848a347f99e1019d716ad35a836f0bbd1aa6
Christoph Rueff <christoph.rueff@gmail.com>
2017-11-24 00:05:21 +0100

    implement social demo UI

---
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/tests/SocialDemoActivity.java
@@ -0,0 +1,15 @@
+package ch.epfl.sweng.fiktion.views.tests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.epfl.sweng.fiktion.R;
+
+public class SocialDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_demo);
+    }
+}
