45fb6a41f049e6b2bb39994e7da5b3eff292dddd
Christoph Rueff <christoph.rueff@gmail.com>
2017-11-30 16:52:23 +0100

    Add social interactions method signatures in UserController

--- app/src/main/java/ch/epfl/sweng/fiktion/controllers/UserController.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/controllers/UserController.java
@@ -1,5 +1,7 @@
 package ch.epfl.sweng.fiktion.controllers;
 
+import ch.epfl.sweng.fiktion.models.User;
+
 /**
  * This class defines the user's social interactions methods.
  *
@@ -7,4 +9,20 @@ package ch.epfl.sweng.fiktion.controllers;
  */
 
 public class UserController {
+
+    public void sendFriendResquest(User localUser, String userIdTo) {
+
+    }
+
+    public void acceptFriendRequest(User localUser, String requestId) {
+
+    }
+
+    public void ignoreFriendRequest(User localUser, String requestId) {
+
+    }
+
+    public void removeFromFriendList(User localUser, String friendId) {
+
+    }
 }
