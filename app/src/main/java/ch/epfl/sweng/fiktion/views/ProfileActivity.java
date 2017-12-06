380bb755a99652eee893d49304626d8b6499135e
Alexandre Chau <alexandre.chau@epfl.ch>
2017-12-06 17:14:25 +0900

    Fix db call, attempt verify auth (bug fails on cancel)

--- app/src/main/java/ch/epfl/sweng/fiktion/views/ProfileActivity.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/views/ProfileActivity.java
@@ -2,16 +2,15 @@ package ch.epfl.sweng.fiktion.views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import ch.epfl.sweng.fiktion.R;
 import ch.epfl.sweng.fiktion.models.User;
-import ch.epfl.sweng.fiktion.providers.AuthSingleton;
+import ch.epfl.sweng.fiktion.providers.AuthProvider;
 import ch.epfl.sweng.fiktion.providers.DatabaseProvider;
-import ch.epfl.sweng.fiktion.providers.DatabaseSingleton;
 import ch.epfl.sweng.fiktion.views.parents.MenuDrawerActivity;
+import ch.epfl.sweng.fiktion.views.utils.AuthenticationChecks;
 
 /**
  * Profile activity class
@@ -33,26 +32,27 @@ public class ProfileActivity extends MenuDrawerActivity {
         includeLayout = R.layout.activity_profile;
         super.onCreate(savedInstanceState);
 
+        // check if user is connected and has a valid account
+        AuthenticationChecks.checkAuthState(this);
+        // check if user's account is verified, otherwise prompt verification and/or refresh
+        if (!AuthProvider.getInstance().isEmailVerified()) {
+            return;
+        }
+
         //create user infos fields
         username = (TextView) findViewById(R.id.username);
-        realInfos = (TextView)findViewById(R.id.userRealInfos);
+        realInfos = (TextView) findViewById(R.id.userRealInfos);
         country = (TextView) findViewById(R.id.userCountry);
         //create user lists fields
-        visitedCount = (TextView)findViewById(R.id.visitedCount);
-        favouriteCount = (TextView)findViewById(R.id.savedCount);
-        pictureCount = (TextView)findViewById(R.id.photosCount);
+        visitedCount = (TextView) findViewById(R.id.visitedCount);
+        favouriteCount = (TextView) findViewById(R.id.savedCount);
+        pictureCount = (TextView) findViewById(R.id.photosCount);
         //create user profile images fields
-        profilePicture = (ImageView)findViewById(R.id.userProfilePicture);
-        profileBanner = (ImageView)findViewById(R.id.userBanner);
+        profilePicture = (ImageView) findViewById(R.id.userProfilePicture);
+        profileBanner = (ImageView) findViewById(R.id.userBanner);
 
-
-    }
-
-    @Override
-    public void onStart(){
-        super.onStart();
-
-        AuthSingleton.auth.getCurrentUser(DatabaseSingleton.database, new DatabaseProvider.GetUserListener() {
+        // get user infos
+        AuthProvider.getInstance().getCurrentUser(new DatabaseProvider.GetUserListener() {
             @Override
             public void onSuccess(User currUser) {
                 user = currUser;
@@ -60,14 +60,14 @@ public class ProfileActivity extends MenuDrawerActivity {
                 //TODO : implement these in class User and retrieve them here
                 realInfos.setText("Real informations not implemented");
                 country.setText("Country not implemented");
-                visitedCount.setText(user.getVisited().size()+" visited");
-                favouriteCount.setText(user.getFavourites().size()+" favoured");
+                visitedCount.setText(user.getVisited().size() + " visited");
+                favouriteCount.setText(user.getFavourites().size() + " favoured");
                 pictureCount.setText("Photo count not implemented");
 
             }
 
             @Override
-            public void onDoesntExist(){
+            public void onDoesntExist() {
                 redirectToLogin();
             }
 
@@ -78,31 +78,11 @@ public class ProfileActivity extends MenuDrawerActivity {
         });
     }
 
-
     /**
      * Prompt the User with a sign in
      */
     public void redirectToLogin() {
         Intent i = new Intent(this, SignInActivity.class);
         startActivityForResult(i, SIGNIN_REQUEST);
-    }
-
-    /**
-     * Triggered by result from another activity launched from here
-     *
-     * @param requestCode The code of the requested activity
-     * @param resultCode  The result code
-     * @param data        The extra data from the activity
-     */
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case SIGNIN_REQUEST: {
-                if (resultCode == RESULT_OK) {
-                    this.recreate();
-                }
-                break;
-            }
-        }
     }
 }
