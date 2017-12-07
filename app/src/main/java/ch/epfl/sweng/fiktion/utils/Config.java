c88b32825dc068d5657e13be39aa21dc33b1303f
Rodrigo Granja <rodrigo.soaresgranja@epfl.ch>
2017-12-07 17:58:28 +0100

    Add search radius constants

--- app/src/main/java/ch/epfl/sweng/fiktion/utils/Config.java
+++ app/src/main/java/ch/epfl/sweng/fiktion/utils/Config.java
@@ -10,7 +10,11 @@ import ch.epfl.sweng.fiktion.models.User;
  */
 public class Config {
 
+    //SearchRadius values
     public static final int DEFAULT_SEARCH_RADIUS = 20;
+    public static final int MIN_SEARCH_RADIUS = 1;
+    public static final int MAX_SEARCH_RADIUS = 200;
+    // boolean value for testing purposes
     public static boolean TEST_MODE = false;
     public static Settings defaultSettings = new Settings(DEFAULT_SEARCH_RADIUS);
 }
