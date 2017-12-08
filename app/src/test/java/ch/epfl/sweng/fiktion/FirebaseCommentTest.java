df3a19a5075ecbba6661ad572c217dbdd7a6d9d1
pedro <pedro.torresdacunha@epfl.ch>
2017-12-08 06:01:41 +0100

    correct getCommentVoteOfUserTest

--- app/src/test/java/ch/epfl/sweng/fiktion/FirebaseCommentTest.java
+++ app/src/test/java/ch/epfl/sweng/fiktion/FirebaseCommentTest.java
@@ -176,6 +176,7 @@ public class FirebaseCommentTest {
     @Test
     public void getCommentVoteOfUserTest() {
         ArgumentCaptor<ValueEventListener> vel = ArgumentCaptor.forClass(ValueEventListener.class);
+        doNothing().when(dbRef).addListenerForSingleValueEvent(vel.capture());
 
         final Mutable<String> result = new Mutable<>("");
 
